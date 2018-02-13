5df327e1216e19593ff2d3910a2d16a27b915e23
Manuel Jiménez <man.jime@gmail.com>
2018-02-13 09:07:13 -0500

    ajustes para hacer el zip opcional

--- base64util/src/main/java/com/dubai/utils/Base64Util.java
+++ base64util/src/main/java/com/dubai/utils/Base64Util.java
@@ -18,9 +18,8 @@ import org.apache.commons.io.IOUtils;
 public class Base64Util {
 
 	public static void main(String[] args) throws IOException {
-		if (args[0].equals("-e")) {
-			String name = FilenameUtils.getName(args[1]) + ".b64";
-
+		if (args[0].startsWith("-e")) {
+			String name = FilenameUtils.getName(args[1]) + ".txt";
 			InputStream input = new FileInputStream(args[1]);
 			OutputStream output = new Base64OutputStream(new FileOutputStream(name));
 			IOUtils.copy(input, output);
@@ -28,28 +27,35 @@ public class Base64Util {
 			input.close();
 			System.out.println(args[1] + " encoded!");
 
-			FileOutputStream zip = new FileOutputStream(name + "z");
-			ZipOutputStream zos = new ZipOutputStream(zip, Charset.forName("UTF-8"));
-			ZipEntry zipEntry = new ZipEntry(name);
-			try {
-				zos.putNextEntry(zipEntry);
-				IOUtils.copy(new FileInputStream(name), zos);
-				zos.closeEntry();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
+			if (args[0].startsWith("-ez")) {
+				FileOutputStream zip = new FileOutputStream(name + ".zip");
+				ZipOutputStream zos = new ZipOutputStream(zip, Charset.forName("UTF-8"));
+				ZipEntry zipEntry = new ZipEntry(name);
+				try {
+					zos.putNextEntry(zipEntry);
+					IOUtils.copy(new FileInputStream(name), zos);
+					zos.closeEntry();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				zos.close();
+				zip.close();
+				System.out.println(name + " compressed!");
 			}
-			zos.close();
-			zip.close();
-			System.out.println(name + " compressed!");
-		} else if (args[0].equals("-d")) {
-			ZipFile zip = new ZipFile(args[1], Charset.forName("UTF-8"));
-			ZipEntry zipEntry = zip.entries().nextElement();
-			String name = zipEntry.getName();
-			FileOutputStream fos = new FileOutputStream(name);
-			IOUtils.copy(zip.getInputStream(zipEntry), fos);
-			fos.close();
-			zip.close();
-			System.out.println(args[1] + " uncompressed!");
+		} else if (args[0].startsWith("-d")) {
+			String name;
+			if (args[0].startsWith("-dz")) {
+				ZipFile zip = new ZipFile(args[1], Charset.forName("UTF-8"));
+				ZipEntry zipEntry = zip.entries().nextElement();
+				name = zipEntry.getName();
+				FileOutputStream fos = new FileOutputStream(name);
+				IOUtils.copy(zip.getInputStream(zipEntry), fos);
+				fos.close();
+				zip.close();
+				System.out.println(args[1] + " uncompressed!");
+			} else {
+				name = args[1];
+			}
 
 			InputStream input = new Base64InputStream(new FileInputStream(name));
 			OutputStream output = new FileOutputStream(FilenameUtils.getBaseName(name));
